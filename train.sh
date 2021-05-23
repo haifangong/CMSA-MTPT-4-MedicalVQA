2f57e80409440c739b70331b609849cc021d6a85
haifan gong <67967691+haifangong@users.noreply.github.com>
2021-05-23 10:48:56 +0800

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=0 python3 main.py --model CMSA --use_spatial --distmodal  --output saved_models/CMSA-Bio-MTPT --epochs 60
+CUDA_VISIBLE_DEVICES=0 python3 main.py --model CMSA --use_spatial --distmodal  --output saved_models/CMSA-Bio-MTPT --epochs 80
